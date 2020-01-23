fd9f5c8fba92a8ff579ed0538f3cd1023321403a
raphinitum95 <raphaeldlst@gmail.com>
2020-01-22 21:50:47 -0600

    trying to fix date

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -3,7 +3,8 @@ import "./CardDetails.scss"
 
 const CardDetails = (props) => {
     const updatedDate = props.isDate ? props.data : "";
-    const updated = new Date(updatedDate);
+    const updated = new Date(Date.parse(updatedDate.split("T")[0]));
+
     const displayDate = ( ( updated.getMonth() + 1 ) + "/" + updated.getDate() + "/" + updated.getFullYear() );
     return (
         <div className="card-square">
